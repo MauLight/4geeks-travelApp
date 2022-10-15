02b9a0b64894d0d3d7e183a844faad1c30c6d333
Covaar <co.va.ard@gmail.com>
2022-10-15 21:36:53 +0000

    Views

---
+++ src/front/js/views/Profile.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactDOM from "react-dom";
+const Nombre = () => {
+    return ()   }
+export default Nombre;
