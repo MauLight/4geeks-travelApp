ddc575c689a9a69b48d4f563efe34ae730557e5c
Covaar <co.va.ard@gmail.com>
2022-10-25 19:54:27 +0000

    Auth

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ import cloudinary
 
 from api.routes.main import bpMain
 from api.routes.user import bpUser
-from api.routes.auth import bpUsers
+from api.routes.auth import bpAuth 
 from api.routes.galleries import bpGI
 from api.routes.userpicture import bpUP
 
@@ -62,8 +62,8 @@ cloudinary.config(
 # Add all endpoints form the API with a "api" prefix
 # app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(bpMain)
-<<<<<<< HEAD
-app.register_blueprint(bpUsers)
+
+app.register_blueprint(bpAuth)
 app.register_blueprint(bpUser)
 app.register_blueprint(bpAuth)
 app.register_blueprint(bpGI, url_prefix="/api")
