5a0b8b03b11633bd3b5c1df46a528df6f156c19e
MauLight <maulisseluz@gmail.com>
2022-10-25 01:49:39 +0000

    recommendations first version

--- src/front/js/views/Recommendations.js
+++ src/front/js/views/Recommendations.js
@@ -1,6 +1,82 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import ReactDOM from "react-dom";
-const Nombre = () => {
-    return ()   }
-export default Nombre;
+import "./App.css";
+import React, { useEffect, useState } from "react";
+
+const URL_API = "...";
+
+const getFetch = (url = "", options = {}) => {
+  return fetch(url, options);
+};
+
+const style = {
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+};
+
+const CardBody = ({ name, description, image }) => {
+  return (
+    <div className="card mb-5">
+      <img src={image} className="card-img-top" alt="..." style={style} />
+      <div className="card-body">
+        <h3 className="card-text">{name}</h3>
+        <p className="card-text">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+const Loading = () => {
+  return (
+    <div className="spinner-border text-danger" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  );
+};
+
+export default function RecommendationCard() {
+  const [trips, setTrips] = useState(null);
+
+  useEffect(() => {
+    getTrips(URL_API);
+  }, []);
+
+  const getTrips = (
+    url,
+    options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  ) => {
+    getFetch(url, options)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        setTrips(response);
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        {!!trips && trips.results.length > 0 ? (
+          trips.results.map((trip) => {
+            return (
+              <div className="col-md-4 col-sm-4 col-12" key={trip.id}>
+                <CardBody {...trip} />
+              </div>
+            );
+          })
+        ) : (
+          <div className="col-md-12 text-center">
+            <Loading />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
