abc43e940c88cb0163eebaedea3ab88510a4ed61
MauLight <maulisseluz@gmail.com>
2022-11-19 11:57:49 +0000

    activities table added, users table modified to fetch matches and activities

---
+++ src/api/routes/activities.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, jsonify, request
+
+from api.models import Activities
+
+
+bpRec = Blueprint('bpAct', __name__)
+
+
+@bpRec.route('/activities', methods=['GET'])  # type: ignore
+def all_activities():
+    activities = Activities.query.all()
+    activities = list(map(lambda activity: activity.serialize(), activities))
+    return jsonify(activities), 200
+
+
+@bpRec.route('/activities', methods=['POST'])  # type: ignore
+def store_activity():
+    id = request.json.get('id')
+    name = request.json.get('name')
+    url_img = request.json.get('url_img')
+    description = request.json.get('description')
+    activity = request.json.get('activity')
+    city = request.json.get('city')
+    country = request.json.get('country')
+
+    activity = Activities()
+    activity.id = id
+    activity.name = name
+    activity.url_img = url_img
+    activity.description = description
+    activity.activity = activity
+    activity.city = city
+    activity.country = country
+    activity.save()
+
+    return jsonify(activity.serialize()), 201
+
+
+@bpRec.route('/activities/<int:id>/delete', methods=['DELETE'])
+def delete_activity(id):
+    activity = Activities.query.get(id)
+    activity.delete()
+    return jsonify({"message": "Activity Deleted!"}), 200
